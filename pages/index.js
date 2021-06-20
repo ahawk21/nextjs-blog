8a63c9ce31b393f275e99f3beaeb343985dc99b6
Ahawkfizz <001.aqh@gmail.com>
2021-06-20 13:07:39 -0500

    deploymentPreviewTest

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home({ allPostsData }) {
         <title >{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Test</p>
+        {/* <p>Test</p> */}
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
